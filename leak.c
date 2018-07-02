27ed142e5778167e323852881e67ca0ec0c69819
sirius <sirius.ustc@gmail.com>
2018-07-02 21:32:25 +0800

    did some modification to leak.c

--- leak.c
+++ leak.c
@@ -1,18 +1,22 @@
-//#include "libkdump.h"
+// include works of https://github.com/IAIK/meltdown
+#include "libkdump/libkdump.h"
+#include <sched.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #define LEN 11
 
-const char *secret = "You got the flag!! flag is: flag{";
+char *secret = "You got the flag!! flag is: flag{";
 
 int main(int argc, char const *argv[])
 {
 	int i;
-//    libkdump_config_t config;
-//    config = libkdump_config_t();
-//    libkdump_init(config);
+    int len;
+    char *message;
+    libkdump_config_t config;
+    config = libkdump_get_autoconfig();
+    libkdump_init(config);
 
     srand(time(NULL));
     char flag[LEN];
@@ -22,7 +26,31 @@ int main(int argc, char const *argv[])
     }
     flag[LEN-2] = '}';
     flag[LEN-1] = '\n';
-    printf("%s%s", secret, flag);
+    printf("Secret: %s%s", secret, flag);
+    sprintf(message, "%s%s", secret, flag);
+    len = strlen(message);
+
+    size_t paddr = libkdump_virt_to_phys((size_t)secret);
+    if (!paddr) 
+    {
+        printf("Program requires root privileges (or read access to /proc/<pid>/pagemap)!\n");
+        libkdump_cleanup();
+        exit(1);
+    }
+
+    printf("Physical address of secret: %zx\n", paddr);
+    printf("Exit with Ctrl+C if you are done reading the secret\n");
+    while (1)
+    {
+        volatile size_t dummy = 0, i;
+        for (i = 0; i < len; i++)
+        {
+            dummy += message[i];
+        }
+        sched_yield();
+    }
+
+    libkdump_cleanup();
     /* code */
     return 0;
 }
